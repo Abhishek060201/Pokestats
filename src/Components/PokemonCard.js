64c6070709a150bbaac45756625e7a8a7e553313
Abhishek Tiwari <65460196+Abhishek060201@users.noreply.github.com>
2022-05-10 19:39:45 +0530

    Update PokemonCard.js

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -1,34 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const PokemonCard = ({ pokemonName, pokemonType, pokemonImageUrl, pokemonHeight, pokemonWeight }) => {
-  const [prevPokemonType, setPrevPokemonType] = useState('a')
+const PokemonCard = ({
+  pokemonName,
+  pokemonType,
+  pokemonImageUrl,
+  pokemonHeight,
+  pokemonWeight,
+}) => {
+  const [prevPokemonType, setPrevPokemonType] = useState("a");
 
   useEffect(() => {
-    const theme = document.querySelector('.card')
-    theme.classList.add(pokemonType)
-    theme.classList.remove(prevPokemonType)
-    setPrevPokemonType(pokemonType)
-  }, [pokemonType])
+    const theme = document.querySelector(".card");
+    theme.classList.add(pokemonType);
+    theme.classList.remove(prevPokemonType);
+    setPrevPokemonType(pokemonType);
+  }, [pokemonType]);
 
   const capitalize = (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  }
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
 
   return (
-    <div className='card'>
-      <div className='image-container'>
-        <img alt='Image not available' src={pokemonImageUrl} height='200px' width='250' />
+    <div className="card">
+      <div className="image-container">
+        <img
+          alt="Image not available"
+          src={pokemonImageUrl}
+          height="200px"
+          width="250"
+        />
       </div>
-      <div className='name' >{capitalize(pokemonName)}</div>
-      <div className='type'>{capitalize(pokemonType)}<span> type</span></div>
-      <div className='height'>{"Height: "}
-        {(Math.round(pokemonHeight * 10) >= 100) ? Math.round(pokemonHeight * 0.1) + " m" : Math.round(pokemonHeight * 10) + " cm"}
+      <div className="card-details">
+        <div className="name">{capitalize(pokemonName)}</div>
+        <div className="type">
+          {capitalize(pokemonType)}
+          <span> type</span>
+        </div>
+        <div className="height">
+          {"Height: "}
+          {Math.round(pokemonHeight * 10) >= 100
+            ? Math.round(pokemonHeight * 0.1) + " m"
+            : Math.round(pokemonHeight * 10) + " cm"}
+        </div>
+        <div className="height">
+          {"Weight: "}
+          {pokemonWeight / 10 < 1
+            ? Math.round(pokemonWeight * 100) + " gm"
+            : Math.round(pokemonWeight / 10) + " kg"}
+        </div>
       </div>
-      <div className='height'>{"Weight: "}
-        {((pokemonWeight / 10) < 1) ? Math.round(pokemonWeight * 100) + " gm" : Math.round(pokemonWeight / 10) + " kg"}
-      </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
